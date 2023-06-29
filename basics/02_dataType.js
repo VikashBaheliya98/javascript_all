fb02fe6e13b35af27f76c7fa1410691bc3244a62
VikashBaheliya98 <138164327+VikashBaheliya98@users.noreply.github.com>
2023-06-29 21:54:49 +0000

    datatypes

---
+++ basics/02_dataType.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3); we are using node.js not browser
+
+console.log( 3
+    +
+    3)   // wrong represantation because code readibility should be high
+
+    console.log("vikash baheliya")
+
+    let name = "vikash"
+    let age  = 25
+    let isLoggedIn = true
+
+    // so there are 2 types or datatype
+    // 1. primitive datatype   2. nonprimitive or referenced datatype
+
+    //primitive datatype are as follows
+
+    // string = "vikash"
+    // number = 544 , range vary from 2 to power 52
+    // boolean = true or false
+    // bigInt 
+    // null = stand alone value
+    // undefined 
+    // symbol = unique
+    
+
+    // primitive datatype are as follows
+
+    // object
+
+
+    console.log(typeof undefined);  // undefined
+
+    console.log(typeof null);   // object
