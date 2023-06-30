4e3b04c77036ebab2a769dd917fa2d749050a178
VikashBaheliya98 <138164327+VikashBaheliya98@users.noreply.github.com>
2023-06-30 11:13:36 +0000

    conversionOperations

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33aaa"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let score1 = "vikash";
+let value = Number(score1)
+// console.log(typeof value)
+// console.log(value)
+
+// "33" => 33 number
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "vikash"
+
+let boolean = Boolean(isLoggedIn)
+// console.log(boolean)
+
+// 1 => true; 0 => false; 
+// "" => false; 
+// "vikash" => true;
+
+
+let isNumber = 33
+
+let isString = String(isNumber);
+console.log(isString);
+console.log(typeof isString);
