30e2fa11aa21061995c71f7ad6141a58ca97b551
VikashBaheliya98 <138164327+VikashBaheliya98@users.noreply.github.com>
2023-06-30 12:02:04 +0000

    conversionOperation

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -30,5 +30,48 @@ let boolean = Boolean(isLoggedIn)
 let isNumber = 33
 
 let isString = String(isNumber);
-console.log(isString);
-console.log(typeof isString);
+// console.log(isString);
+// console.log(typeof isString);
+
+
+// ******************** Operations **********************
+
+let value1 = 3
+let negvalue = -value1
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "vikash"
+let str2 = " baheliya"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+
+
+// console.log((3 + 4) * 5 % 3 );
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
